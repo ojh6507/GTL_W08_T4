fcae762fb4db224d2606fe97a522027ece9f11de
Kimu-Chan <woochankim0303@gmail.com>
2025-04-26 17:51:36 +0900

    fix: Ambient Light

--- EngineSIU/EngineSIU/Shaders/Light.hlsl
+++ EngineSIU/EngineSIU/Shaders/Light.hlsl
@@ -441,7 +441,7 @@ float4 Lighting(float3 WorldPosition, float3 WorldNormal, float3 WorldViewPositi
     [unroll(MAX_AMBIENT_LIGHT)]
     for (int l = 0; l < AmbientLightsCount; l++)
     {
-        FinalColor += float4(AmbientLights[l].AmbientColor.rgb, 0.0);
+        FinalColor += float4(AmbientLights[l].AmbientColor.rgb * DiffuseColor, 0.0);
         FinalColor.a = 1.0;
     }
     
